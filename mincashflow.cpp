4ac0b4c49419db69a7aa4087f7ac814503968dc4
Yatin Kapoor <yatinkapoor200@gmail.com>
2023-07-02 10:41:18 +0530

    first commit

---
+++ mincashflow.cpp
@@ -0,0 +1,83 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void cashflowmin(vector<int> &payment)
+{
+    int m = payment.size();
+    while(true){
+
+        int maxcredit = 0, maxdebit = 0, maxi, mini;
+
+        for(int i = 0 ; i < m ; i++){                      //finding max credit value and index
+            
+            if(payment[i] > maxcredit){
+                
+                maxcredit = payment[i];
+                maxi = i;
+            }
+        }               
+        for(int i = 0 ; i < m ; i++){                     // finding max debit value and index
+                              
+            if(payment[i] < maxdebit){
+                
+                maxdebit = payment[i];
+                mini = i;
+            }
+        }
+
+
+        if(maxcredit == 0 && maxdebit == 0){
+            
+            break;
+        }
+
+        int temp = min(abs(maxdebit), maxcredit);
+
+        payment[maxi] -= temp;
+        payment[mini] += temp;
+
+        cout << "Friend " << mini + 1 << " paid " << temp << " rupees to Friend " << maxi + 1 << "\n";
+
+    }
+
+}
+
+void netpayment(vector<vector<int>> &cashmatrix)
+{
+    int m = cashmatrix.size();
+    vector<int> payment(m, 0);
+    
+    for(int i = 0 ; i < m ; i++){
+        for(int j = 0 ; j < m ; j++){
+
+           payment[i] += cashmatrix[j][i] - cashmatrix[i][j];
+           
+        }
+        
+    }
+
+    cashflowmin(payment);
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+    vector<vector<int>> cashmatrix;
+    for(int i = 0 ; i < n ; i++){
+        
+        vector<int> tempvec;
+        for(int j = 0 ; j < n ; j++){
+            
+            int temp;
+            cin >> temp;
+            tempvec.push_back(temp);
+            
+        }
+        cashmatrix.push_back(tempvec);
+    }
+    cout << cashmatrix.size() << "\n";
+    netpayment(cashmatrix);
+    
+    return 0;
+}
