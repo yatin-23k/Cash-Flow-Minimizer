b8216f9411e862c1cbb748a4d5b220a5b2b602e3
Yatin Kapoor <yatinkapoor200@gmail.com>
2023-07-02 10:59:51 +0530

    adjust indentation

--- mincashflow.cpp
+++ mincashflow.cpp
@@ -1,33 +1,29 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-void cashflowmin(vector<int> &payment)
+void cashFlowMin (vector<int> &payment)
 {
     int m = payment.size();
-    while(true){
+    
+    while (true) {
 
         int maxcredit = 0, maxdebit = 0, maxi, mini;
 
-        for(int i = 0 ; i < m ; i++){                      //finding max credit value and index
-            
-            if(payment[i] > maxcredit){
-                
+        for (int i = 0; i < m; i++) {                      //finding max credit value and index
+            if (payment[i] > maxcredit) {
                 maxcredit = payment[i];
                 maxi = i;
             }
         }               
-        for(int i = 0 ; i < m ; i++){                     // finding max debit value and index
-                              
-            if(payment[i] < maxdebit){
-                
+        
+        for (int i = 0; i < m; i++) {                     // finding max debit value and index
+            if (payment[i] < maxdebit) {
                 maxdebit = payment[i];
                 mini = i;
             }
         }
 
-
-        if(maxcredit == 0 && maxdebit == 0){
-            
+        if (maxcredit == 0 && maxdebit == 0) {
             break;
         }
 
@@ -37,26 +33,21 @@ void cashflowmin(vector<int> &payment)
         payment[mini] += temp;
 
         cout << "Friend " << mini + 1 << " paid " << temp << " rupees to Friend " << maxi + 1 << "\n";
-
     }
-
 }
 
-void netpayment(vector<vector<int>> &cashmatrix)
+void netPayment (vector<vector<int>> &cashmatrix)
 {
     int m = cashmatrix.size();
     vector<int> payment(m, 0);
     
-    for(int i = 0 ; i < m ; i++){
-        for(int j = 0 ; j < m ; j++){
-
-           payment[i] += cashmatrix[j][i] - cashmatrix[i][j];
-           
+    for (int i = 0; i < m; i++) {
+        for (int j = 0; j < m; j++) {
+            payment[i] += cashmatrix[j][i] - cashmatrix[i][j];
         }
-        
     }
 
-    cashflowmin(payment);
+    cashFlowMin(payment);
 }
 
 int main()
@@ -64,20 +55,22 @@ int main()
     int n;
     cin >> n;
     vector<vector<int>> cashmatrix;
-    for(int i = 0 ; i < n ; i++){
-        
+    
+    for (int i = 0; i < n; i++) {
         vector<int> tempvec;
-        for(int j = 0 ; j < n ; j++){
-            
+        
+        for (int j = 0; j < n; j++) {            
             int temp;
             cin >> temp;
-            tempvec.push_back(temp);
-            
+            tempvec.push_back(temp);            
         }
+        
         cashmatrix.push_back(tempvec);
     }
+    
     cout << cashmatrix.size() << "\n";
-    netpayment(cashmatrix);
+    
+    netPayment(cashmatrix);
     
     return 0;
 }
